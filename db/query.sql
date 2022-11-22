09a212783fb2c3d4a2bfc3701bf4991412e5c402
Sean Shin <shin.seang@gmail.com>
2022-11-22 01:10:27 -0500

    added view roles functionality to Application and Query js files

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,7 @@
 SELECT * FROM department;
 SELECT * FROM roles;
 SELECT * FROM employee;
+
+SELECT roles.id AS id, roles.title AS title, roles.salary AS salary, department.name AS department
+FROM roles
+JOIN department ON roles.department_id = department.id;
